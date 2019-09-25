cf238b05f5674eedc4b00852a4f6ed23d9b0d545
sintornillos <rubiancocarlos@gmail.com>
2019-09-25 13:06:35 -0300

    Cambio de pc

--- __test__/numbers.test.js
+++ __test__/numbers.test.js
@@ -1,22 +1,20 @@
-import React from "react";
+// import React from "react";
 // import ReactTestUtils from "react-dom/test-utils";
-import { shallow } from "enzyme";
+// import { shallow } from "enzyme";
 
-import hello from "../index";
+import suma from "../src/sum";
 
-describe("hello", () => {
-  describe("#hello()", function() {
-    it("Debería devolver Hola", function() {
-      const component = shallow(<hello debug />);
-      assert.equal(conver.hello(), "Hola");
-    });
-  });
-});
+// describe("hello", () => {
+//   describe("#hello()", function() {
+//     it("Debería devolver Hola", function() {
+//       const component = shallow(<hello debug />);
+//       assert.equal(conver.hello(), "Hola");
+//     });
+//   });
+// });
 
-describe("Teste de numeros", () => {
-  it("Adding 1 + 1 equals 2", () => {
-    expect(1 + 1).toBe(2);
-  });
+test("Test suma", () => {
+  expect(suma(3, 5)).toBe(8);
 });
 
 // it("Exista un uno ",()=>{
